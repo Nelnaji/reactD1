741761cbbd68c52b01f71f42108b703fe22f94bc
Hiyaro <nawfal.elkhaznagi@gmail.com>
2022-06-21 16:28:58 +0200

    fin exercice 4

---
+++ exercice4/src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class App extends Component{
+  render() {
+    return(
+<div> 
+
+<button className="btn bg-secondary">Test</button>
+<i className="fa-solid fa-angle-left"></i>
+</div>
+    )
+  }
+}
+
+export default App;
