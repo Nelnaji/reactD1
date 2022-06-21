30418dba9c8aed95bf69cd4c020227ce359ec3e1
Hiyaro <nawfal.elkhaznagi@gmail.com>
2022-06-21 16:55:54 +0200

    fin de l'exercice 5

---
+++ exercice5/src/header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Navigation extends React.Component {
+  render(){
+    return(
+<div>
+       Navigation bar
+   <nav>
+    <ul>
+        <li>1</li>
+        <li>1</li>
+        <li>1</li>
+        <li>1</li>
+
+    </ul>
+   </nav>
+      </div>
+    )
+  };
+  }
+
+export default Navigation;
